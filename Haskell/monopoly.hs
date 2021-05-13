8444d6c6412a41de0d793ac9bd6beefec680088f
unknown <crinaldi@frba.utn.edu.ar>
2021-05-12 23:09:34 -0300

    segundo commit de prueba

--- Haskell/monopoly.hs
+++ Haskell/monopoly.hs
@@ -23,7 +23,9 @@ manuel = UnParticipante "Manuel" 500 "Oferente singular" [] [pasarPorElBanco, en
 jugadorParaPrueba :: Participante
 jugadorParaPrueba = UnParticipante "Merli" 500 "Oferente singular" [("Casa",10000),("Depto1",100),("Depto2",120)] [pasarPorElBanco, enojarse, subastar ("Depto1",100), pagarAAccionistas, hacerBerrinchePor ("Depto2",120)]
 
+
 --ACCIONES----------------------------------------------------------------------------------------------------------------
+
 pasarPorElBanco :: Accion
 pasarPorElBanco unJugador = unJugador {cantidadDeDinero = cantidadDeDinero unJugador + 40, tactica = "Comprador Compulsivo"}
 
@@ -38,6 +40,7 @@ subastar unaPropiedad unJugador
     | (tactica unJugador == "Oferente singular") || (tactica unJugador == "Accionista") = unJugador {cantidadDeDinero = cantidadDeDinero unJugador - (snd unaPropiedad), propiedadesCompradas = unaPropiedad : (propiedadesCompradas unJugador)}
 
 --Funciones Auxiliares para Accion cobrarAlquileres-----------------------------------------------------------------------------
+
 esPropiedadBarata :: Propiedad -> Bool
 esPropiedadBarata unaPropiedad = (<150).snd $ unaPropiedad
 
@@ -57,7 +60,6 @@ hacerBerrinchePor :: Propiedad -> Accion
 hacerBerrinchePor unaPropiedad unJugador
     | cantidadDeDinero unJugador >= snd unaPropiedad = unJugador {cantidadDeDinero = cantidadDeDinero unJugador - (snd unaPropiedad), propiedadesCompradas = unaPropiedad : (propiedadesCompradas unJugador)}
     | otherwise = hacerBerrinchePor unaPropiedad (gritar (unJugador {cantidadDeDinero = cantidadDeDinero unJugador + 10})) 
-
 
 ultimaRonda :: Participante -> Accion
 ultimaRonda unJugador = foldr1 (.) (acciones unJugador)
